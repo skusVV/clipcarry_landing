281414fd6b777c659f69b4eaecf17f079bcc5d06
vskus <vskus@softserveinc.com>
2022-07-13 15:47:47 +0300

    increased timeout

--- src/http.ts
+++ src/http.ts
@@ -3,7 +3,7 @@ import { API_URL } from './constants';
 
 export const http = axios.create({
     baseURL: `${API_URL}/api`,
-    timeout: 1000
+    timeout: 600000
 });
 
 export const setToken = token => ({ headers: {'x-access-token': token}});
